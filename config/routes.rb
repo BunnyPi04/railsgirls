d4c9f8cc7e45579afdcf2a9ef35720bc61f78c7b
bunnypi <hangpt248@gmail.com>
2017-09-16 07:59:42 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/info'
+
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+root to: redirect('/ideas')
+end
